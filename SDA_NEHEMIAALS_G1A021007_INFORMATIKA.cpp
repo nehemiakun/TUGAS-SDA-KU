f769dbcc71ccf95c5bba7f63dd20f9ca14ed2e91
Nehemia_dst <99037710+nehemiakun@users.noreply.github.com>
2022-02-05 09:39:32 -0800

    NEHEMIAALS_G1A021007_SDA_INFORMATIKA
    source code
    #include <iostream>
    
    using namespace std;
    
    int main(){
    
      int a,t,b;
      float luas;
    
      cout << "50: ";
      cin >> a;
      cout << "20: ";
      cin >> t;
      cout << "10: ";
      cin >> b;
    
      luas = 0.5*a*t*b;
      cout << "Luas segitiga Adalah  " << luas << endl;
    
      return 0;
    }
    
    penjelasan source code
    1.int untuk menyatakan bilangan bulat
    2.float untuk menyatakan bilangan desimal
    3.count untuk menghitung jumlah entri
    4.cin untuk menerima data perintah

---
+++ SDA_NEHEMIAALS_G1A021007_INFORMATIKA.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+  int a,t,b;
+  float luas;
+
+  cout << "50: ";
+  cin >> a;
+  cout << "20: ";
+  cin >> t;
+  cout << "10: ";
+  cin >> b;
+
+  luas = 0.5*a*t*b;
+  cout << "Luas segitiga Adalah  " << luas << endl;
+
+  return 0;
+}
